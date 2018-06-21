d41d95e19c682ad24c5ec28564f2f508a20102aa
yoegarrrcia@gmail.com <yoegarrrcia@gmail.com>
2018-06-21 00:18:15 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,48 +1,9 @@
-function netExplosiveWeight (qty,exp,weight)
+function ass (n)
 
 {
-  if (weight === false && exp.weight === false)
-   {return 'You need to enter a weight';}
 
-  else if (weight === false && exp.weight === true)
-  {return exp.weight * qty * exp.reFactor;}
-
-  else if (weight === true && exp.weight === true)
-    {return weight * qty * exp.reFactor;}
-
-  else if (weight === true && exp.weight === false)
-    {return weight * qty * exp.reFactor;}
-
+return n*n
+  
 }
 
-c4 = {name: 'C4', weight: 1.25, reFactor: 1.34 };
-
-ammoniumNitrate = {name: 'Ammonium nitrate', reFactor: 0.42 };
-
-petn = {name: 'PETN', reFactor: 1.66};
-
-rdx = {name: 'RDX', reFactor: 1.60};
-
-tnt = {name: 'TNT', reFactor: 1};
-
-tetryl = {name: 'Tetryl', reFactor: 1.25};
-
-nitroglycerin = {name: 'Nitroglycerin', reFactor: 1.50 };
-
-blackPowder = {name: 'Black powder', reFactor: 0.55};
-
-amatol8020 = {name: 'Amatol 80/20', reFactor: 1.17 };
-
-compositionB = {name: 'Composition B', reFactor: 1.35};
-
-compositionH6 = {name: 'CompositionH6', reFactor: 1.33 };
-
-m1Dynamite = {name: 'M1 dynamite ', reFactor: 0.92};
-
-detonatingCord= {name: 'Detonating cord', reFactor: 1.66};
-
-sheetExplosiveM186 = {name: 'Sheet explosive(M186)', reFactor: 1.14 };
-
-bangaloreTorpedoM1A2 = {name: 'Bangaloretorpedo, M1A2', reFactor: 1.17 };
-
-console.log(netExplosiveWeight(1, c4, 3));
+console.log(ass(9))
